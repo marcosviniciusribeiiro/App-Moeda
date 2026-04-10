318ae6001ea9b41f4458c79b04f344123cee67e4
MarcosVinicius <Marcosvini.rg@gmail.com>
2026-04-10 09:04:52 -0300

    Update Cotacao.java

--- app/src/main/java/com/exemplo/ewm/appmoeda/Cotacao.java
+++ app/src/main/java/com/exemplo/ewm/appmoeda/Cotacao.java
@@ -4,9 +4,14 @@ import com.google.gson.annotations.SerializedName;
 
 public class Cotacao {
     @SerializedName("bid")
-    private String valor;
+    private final String valor;
+
+    public Cotacao(String valor) {
+        this.valor = valor;
+    }
 
     public String getValor() {
         return valor;
     }
+
 }
